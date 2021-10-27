b31cb84ef94f50dddf6e8af051809cea180b333d
SlMibo <mdelosab@gmail.com>
2021-10-27 16:06:57 -0300

    jwt valid

--- src/routes/professionals.routes.js
+++ src/routes/professionals.routes.js
@@ -23,8 +23,8 @@ route.get("/", getProfesionales);
 route.get("/:id", getProfesional);
 
 // privadas // [validar_jwt, verificarActivo],
-route.post("/", post_middlewares_professional, createProfesional);
-route.put("/:id", update_middlewares_professional, updateProfesional);
+route.post("/", [validar_jwt, post_middlewares_professional], createProfesional);
+route.put("/:id", [validar_jwt, update_middlewares_professional], updateProfesional);
 // route.put("/password/:id", [validar_jwt, verificarActivo], updateProfesional);
 route.delete("/:id", [validar_jwt, verificarActivo], deleteProfesional);
 
