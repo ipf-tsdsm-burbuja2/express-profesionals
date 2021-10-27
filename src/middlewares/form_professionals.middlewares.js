c8f737d8eb240d914caf1c36fd94f22a86d989b8
SlMibo <mdelosab@gmail.com>
2021-10-27 14:54:01 -0300

    config middlewares and security

--- src/middlewares/form_professionals.middlewares.js
+++ src/middlewares/form_professionals.middlewares.js
@@ -7,13 +7,13 @@ const {
 // Lista de middlewares para el post del profesional
 const post_middlewares_professional = [
   professionalExtractAtributes,
-  body("fullname", "El nombre no debe contener números o signos")
-    .isAscii("es-ES", { ignore: " " })
-    .isLength({ min: 10, max: 150 }),
-  // body("dni", "El formato del dni es incorrecto").isNumeric(),
-  // body("address", "En la dirección sólo se permiten letras.").isAscii({ ignore: " "}),
-  // body("email", "El email ingresado no contiene un formato correcto").isEmail(),
-  // body("phone", "No es un móvil válido").isNumeric(),
+  body("personal_info.fullname", "El nombre no debe contener números o signos")
+    .isAlpha()
+    .isLength({ min: 5, max: 150 }),
+  body("personal_info.dni", "El formato del dni es incorrecto").isLength({min: 8}),
+  body("personal_info.address", "No es una dirección válida.").isAscii(),
+  body("contact_info.email", "El email ingresado no contiene un formato correcto").isEmail(),
+  body("contact_info.phone", "No es un móvil válido").isMobilePhone(),
 
   showErrors,
 ];
